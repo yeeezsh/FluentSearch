4b4b9fe4e278f9f925d982af953155ba4b33f89f
yee2542 <yee2542gtl@gmail.com>
2021-05-17 22:57:20 +0700

    add ns force remove script

---
+++ kubernetes/script/ns-force-remove.bash
@@ -0,0 +1,4 @@
+NAMESPACE=fluentsearch-bff
+kubectl get namespace $NAMESPACE -o json > $NAMESPACE.json
+sed -i -e 's/"kubernetes"//' $NAMESPACE.json
+kubectl replace --raw "/api/v1/namespaces/$NAMESPACE/finalize" -f ./$NAMESPACE.json
