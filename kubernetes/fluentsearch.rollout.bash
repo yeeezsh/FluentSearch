bddb84ab94186fe168dc71ab50460d3759599822
yee2542 <yee2542gtl@gmail.com>
2021-05-23 03:20:54 +0700

    add fluentsearch rollout script

---
+++ kubernetes/fluentsearch.rollout.bash
@@ -0,0 +1,3 @@
+kubectl rollout restart -n fluentsearch-fe deployment fluentsearch-fe
+kubectl rollout restart -n fluentsearch-bff deployment fluentsearch-bff
+kubectl rollout restart -n fluentsearch-storage deployment fluentsearch-storage
