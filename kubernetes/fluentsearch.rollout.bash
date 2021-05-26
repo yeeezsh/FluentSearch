43798bcc7995d4ad04c411f3482f1f9166e5385e
yee2542 <yee2542gtl@gmail.com>
2021-05-26 07:06:17 +0700

    add rollout command

--- kubernetes/fluentsearch.rollout.bash
+++ kubernetes/fluentsearch.rollout.bash
@@ -1,3 +1,5 @@
 kubectl rollout restart -n fluentsearch-fe deployment fluentsearch-fe
 kubectl rollout restart -n fluentsearch-bff deployment fluentsearch-bff
 kubectl rollout restart -n fluentsearch-storage deployment fluentsearch-storage
+kubectl rollout restart -n fluentsearch-api-federation deployment fluentsearch-api-federation
+kubectl rollout restart -n fluentsearch-admission deployment fluentsearch-admission
