5344b8a5b5c289c2c7abe486d196557aacf7fd98
yee2542 <yee2542gtl@gmail.com>
2021-05-31 07:53:38 +0700

    change federation rollout order

--- kubernetes/fluentsearch.rollout.bash
+++ kubernetes/fluentsearch.rollout.bash
@@ -1,7 +1,7 @@
 kubectl rollout restart -n fluentsearch-fe deployment fluentsearch-fe
 kubectl rollout restart -n fluentsearch-bff deployment fluentsearch-bff
 kubectl rollout restart -n fluentsearch-storage deployment fluentsearch-storage
-kubectl rollout restart -n fluentsearch-api-federation deployment fluentsearch-api-federation
 kubectl rollout restart -n fluentsearch-admission deployment fluentsearch-admission
 kubectl rollout restart -n fluentsearch-spotlight deployment fluentsearch-spotlight
 kubectl rollout restart -n fluentsearch-ml deployment fluentsearch-ml
+kubectl rollout restart -n fluentsearch-api-federation deployment fluentsearch-api-federation
